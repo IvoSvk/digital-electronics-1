648db0334c89da2db31cd317e48249f90d7a5b17
IvoSvk <Ivo.zabavagraduje@gmail.com>
2023-02-26 18:09:35 +0100

    added multiplexer

--- 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
+++ 03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_3bit_4to1.vhd
@@ -1,7 +1,26 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 26.02.2023 18:00:40
+-- Design Name: 
+-- Module Name: tb_mux_3bit_4to1 - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
 library ieee;
-use ieee.math_real.uniform;
-use ieee.math_real.floor;
-use ieee.numeric_std.all;
+
 
 use ieee.std_logic_1164.all;
 
@@ -22,22 +41,19 @@ architecture testbench of tb_mux_3bit_4to1 is
     signal s_b           : std_logic_vector(3 - 1 downto 0);
     signal s_c           : std_logic_vector(3 - 1 downto 0);
     signal s_d           : std_logic_vector(3 - 1 downto 0);
-    
-    signal s_si          : std_logic_vector(3 - 1 downto 0);
-    
-    signal s_f_out       : std_logic_vector(3 - 1 downto 0);
+    signal s_sel         : std_logic_vector(2 - 1 downto 0); -- dvoubitová adresa    
+    signal s_f_o         : std_logic_vector(3 - 1 downto 0);
     
 begin
-    -- Connecting testbench signals with comparator_3bit entity (Unit Under Test)
-    uut_comparator_3bit : entity work.mux_3bit_4to1
+    -- Connecting testbench signals with comparator_2bit entity (Unit Under Test)
+    uut_comparator_2bit : entity work.mux_3bit_4to1
         port map(
             a_i     => s_a,
             b_i     => s_b,
             c_i     => s_c,
             d_i     => s_d,      
-            
-            s_i     => s_si,
-            f_o     => s_f_out
+            sel_i   => s_sel,
+            f_o     => s_f_o
         );
 
     --------------------------------------------------------------------
@@ -45,70 +61,25 @@ begin
     --------------------------------------------------------------------
     p_stimulus : process
     begin
-        -- Report a note at the begining of stimulus process
-        report "Stimulus process started. ---------------------------------------" severity note;
-        s_a     <= "011";
-        s_b     <= "010";
-        s_c     <= "001";
-        s_d     <= "000";
-        
-        s_si    <= "000";
-        wait for 100 ns;
-        assert (s_f_out = s_a)
-        report "Test failed for input combination: 00" severity error;        
-    	wait for 100 ns;
 
-        s_si    <= "001";
-        wait for 100 ns;
-        assert (s_f_out = s_b)
-        report "Test failed for input combination: 01" severity error;        
-    	wait for 100 ns;
-
-        s_si    <= "010";
-        wait for 100 ns;
-        assert (s_f_out = s_c)
-        report "Test failed for input combination: 10" severity error;        
-    	wait for 100 ns;
-
-        s_si    <= "011";
-        wait for 100 ns;
-        assert (s_f_out = s_d)
-        report "Test failed for input combination: 11" severity error;        
-    	wait for 100 ns;
-    	
-    	-- test for another inputs
-    	
-    	s_a     <= "001";
-        s_b     <= "011";
-        s_c     <= "000";
+        s_a     <= "111";
+        s_b     <= "101";
+        s_c     <= "101";
         s_d     <= "010";
         
-        s_si    <= "000";
+        s_sel    <= "00";
         wait for 100 ns;
-        assert (s_f_out = s_a)
-        report "Test failed for input combination: 00" severity error;        
-    	wait for 100 ns;
-
-        s_si    <= "001";
+        
+        s_sel    <= "01";
         wait for 100 ns;
-        assert (s_f_out = s_b)
-        report "Test failed for input combination: 01" severity error;        
-    	wait for 100 ns;
-
-        s_si    <= "010";
+        
+        s_sel   <= "10";
         wait for 100 ns;
-        assert (s_f_out = s_c)
-        report "Test failed for input combination: 10" severity error;        
-    	wait for 100 ns;
-
-        s_si    <= "011";
+        
+        s_sel   <= "11";
         wait for 100 ns;
-        assert (s_f_out = s_d)
-        report "Test failed for input combination: 11" severity error;        
-    	wait for 100 ns;
+        
     	
-        -- Report a note at the end of stimulus process
-        report "Stimulus process finished. ---------------------------------------" severity note;
         wait;
     end process p_stimulus;
 
