fb1f11a6b3873fd407a87d58a736be002578f7f3
IvoSvk <Ivo.zabavagraduje@gmail.com>
2023-03-12 22:12:18 +0100

    updated 05-ffs

--- 05-ffs/flip_flops/flip_flops.srcs/sources_1/new/d_ff_rst.vhd
+++ 05-ffs/flip_flops/flip_flops.srcs/sources_1/new/d_ff_rst.vhd
@@ -18,7 +18,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
@@ -32,33 +31,34 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity d_ff_rst is
-    Port ( d : in STD_LOGIC;
+    Port ( clk : in STD_LOGIC;
+           rst : in STD_LOGIC;
+           d : in STD_LOGIC;
            q : out STD_LOGIC;
-           q_bar : out STD_LOGIC;
-           clk : in STD_LOGIC;
-           rst : in STD_LOGIC);
+           q_bar : out STD_LOGIC);
 end d_ff_rst;
 
-architecture behavioral of d_ff_rst is
+architecture Behavioral of d_ff_rst is
 begin
     --------------------------------------------------------
     -- p_d_ff_rst:
-    -- D type flip-flop with a high-active sync reset and
+    -- D type flip-flop with a high-active sync reset,
     -- rising-edge clk.
     -- q(n+1) = d
     --------------------------------------------------------
-    p_d_ff_rst : process (clk) is
+    p_d_ff_rst : process(clk)
     begin
         if rising_edge(clk) then  -- Synchronous process
 
             -- USE HIGH-ACTIVE RESET HERE
             if (rst = '1') then
-                q     <= '1';
+                q <= '0';
                 q_bar <= '1';
-                else
-               q     <= d;
+            else
+                q     <= d;
                 q_bar <= not d;
+            end if;
+                
         end if;
-          end if;
     end process p_d_ff_rst;
-end architecture behavioral;
+end architecture Behavioral;
